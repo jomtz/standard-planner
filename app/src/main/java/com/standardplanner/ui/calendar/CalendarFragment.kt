ea31e6aabab58c403fc5eb6131c4580c74793888
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2022-03-07 11:06:47 -0500

    Add calendar view

--- app/src/main/java/com/standardplanner/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/standardplanner/ui/calendar/CalendarFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.standardplanner.databinding.FragmentCalendarBinding
+
 
 class CalendarFragment : Fragment() {
 
@@ -28,17 +28,27 @@ class CalendarFragment : Fragment() {
             ViewModelProvider(this).get(CalendarViewModel::class.java)
 
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textCalendar
-        calendarViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+//        val textView: TextView = binding.textCalendar
+//        calendarViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        setupCalendar()
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupCalendar() {
+        binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(this.requireContext(), msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
